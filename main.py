3c76d37e12e5445aba1405835ee63db5cd6fb4c3
Kris Rogos <krisrogos@gmail.com>
2018-09-13 00:07:18 +0100

    2018_09_13_0006: Added a simple image, anchored window to top corner of the main display.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame
 
 
@@ -5,22 +7,30 @@ class App:
     def __init__(self):
         self._running = True
         self._display_surf = None
+        self._image_surf = None
         self.size = self.weight, self.height = 720, 1280
 
     def on_init(self):
-        pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE)
-        self._running = True
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 30) # window origin
+        pygame.init() # start the pygame engine
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE) # window, sized to 720x1280 by default
+        pygame.display.set_caption("PyBlockWall") # window title
+        self.clock = pygame.time.Clock()  # engine clock
+        self._running = True # game state
 
-    def on_event(self, event):
+
+        self._image_surf = pygame.image.load(os.path.join("res", "blockBackground64.png")).convert()
+
+    def on_event(self, event): # event handler
         if event.type == pygame.QUIT:
             self._running = False
 
-    def on_loop(self):
+    def on_loop(self): # game logic per frame
         pass
 
-    def on_render(self):
-        pass
+    def on_render(self): # render logic per frame
+        self._display_surf.blit(self._image_surf, (0, 0))
+        pygame.display.flip() # render the frame
 
     def on_cleanup(self):
         pygame.quit()
@@ -29,13 +39,16 @@ class App:
         if self.on_init() == False:
             self._running = False
 
-            while (self._running):
-                for event in pygame.event.get():
-                    self.on_event(event)
-                self.on_loop()
-                self.on_render()
+        while self._running:
+            for event in pygame.event.get():
+                self.on_event(event)
+                print(event)
+            self.on_loop()
+            self.on_render()
+            self.clock.tick(60)
 
-            self.on_cleanup()
+        self.on_cleanup()
+
 
 if __name__ == "__main__":
     theApp = App()
